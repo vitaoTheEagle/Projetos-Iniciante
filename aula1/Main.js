f275adbb560fb2bf970ae1bef33f7bdae511defa
vitaoTheEagle <huugo.viictor.bueno@gmail.com>
2021-09-18 16:17:29 -0300

    cria pasta alura

--- aula1/Main.js
+++
@@ -1,7 +0,0 @@
-function calc() {
-    var temperaturaCelsius = parseFloat(document.getElementById('valorDigitado').value);
-    
-    temperaturaFahrenheit = (temperaturaCelsius * 1.8) + 32
-document.form.temperatura.value=temperaturaFahrenheit + " F";
-}
-
